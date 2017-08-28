baeb1dc8901c6647f33122e3793b652534958698
pn101 <philipnam101@gmail.com>
2017-08-28 15:12:15 +0100

    init runserver and add home url

--- alpha/alpha/urls.py
+++ alpha/alpha/urls.py
@@ -16,6 +16,13 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from django.http.response import HttpResponse
+
+
+def home(request):
+    return HttpResponse("Hello World")
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', home),
 ]
