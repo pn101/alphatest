da03f5a6fd7245f0c37a78c481676a6c85645fb1
pn101 <philipnam101@gmail.com>
2017-08-28 18:00:46 +0100

    refactor settings:
    create settings module
    create production and development settings

---
+++ alpha/alpha/settings/development.py
@@ -0,0 +1,8 @@
+from .partials import *
+
+
+INSTALLED_APPS += [
+    'debug_toolbar',
+]
+
+INTERNAL_IPS = '127.0.0.1'
---
+++ alpha/alpha/settings/partials/static.py
@@ -0,0 +1,5 @@
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.11/howto/static-files/
+
+STATIC_URL = '/static/'
+
