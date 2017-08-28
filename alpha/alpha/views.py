8a30a4ca1e5f56e3225d5ed09ce176fda4a61f2a
pn101 <philipnam101@gmail.com>
2017-08-28 15:58:23 +0100

    add alpha to installed apps: create home.html and add template.render on home view

--- alpha/alpha/views.py
+++ alpha/alpha/views.py
@@ -1,8 +1,17 @@
 from django.http.response import HttpResponse
+from django.template import loader
 
 
 def home(request):
-    return HttpResponse("Hello World")
+    template = loader.get_template("home.html")
+    return HttpResponse(
+        template.render(
+            {
+                "site_name": "Alpha",
+            },
+            request,
+        )
+    )
 
 def mypage(request, name):
     return HttpResponse("Hello " + name)
